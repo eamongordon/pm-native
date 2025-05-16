b179e987ce91892ce4b1184953cb0c19c977bb23
Eamon Gordon <ekeokigordon@icloud.com>
2025-05-16 09:54:12 -0700

    feat: use work sans font

--- components/ThemedText.tsx
+++ components/ThemedText.tsx
@@ -1,6 +1,6 @@
-import { StyleSheet, Text, type TextProps } from 'react-native';
-
 import { useThemeColor } from '@/hooks/useThemeColor';
+import { useFonts, WorkSans_400Regular, WorkSans_700Bold } from '@expo-google-fonts/work-sans';
+import { StyleSheet, Text, type TextProps } from 'react-native';
 
 export type ThemedTextProps = TextProps & {
   lightColor?: string;
@@ -16,7 +16,16 @@ export function ThemedText({
   ...rest
 }: ThemedTextProps) {
   const color = useThemeColor({ light: lightColor, dark: darkColor }, 'text');
+  
+  const [fontsLoaded] = useFonts({
+    WorkSans_400Regular,
+    WorkSans_700Bold,
+  });
 
+  if (!fontsLoaded) {
+    return null; // Or use <AppLoading /> for a better UX
+  }
+  
   return (
     <Text
       style={[
@@ -37,24 +46,26 @@ const styles = StyleSheet.create({
   default: {
     fontSize: 16,
     lineHeight: 24,
+    fontFamily: 'WorkSans_400Regular',
   },
   defaultSemiBold: {
     fontSize: 16,
     lineHeight: 24,
-    fontWeight: '600',
+    fontFamily: 'WorkSans_700Bold',
   },
   title: {
     fontSize: 32,
-    fontWeight: 'bold',
     lineHeight: 32,
+    fontFamily: 'WorkSans_700Bold',
   },
   subtitle: {
     fontSize: 20,
-    fontWeight: 'bold',
+    fontFamily: 'WorkSans_700Bold',
   },
   link: {
     lineHeight: 30,
     fontSize: 16,
     color: '#0a7ea4',
+    fontFamily: 'WorkSans_400Regular',
   },
 });
