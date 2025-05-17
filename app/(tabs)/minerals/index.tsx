6177f2391e1685b4242f79d9c49bad1a210eedb7
Eamon Gordon <ekeokigordon@icloud.com>
2025-05-16 21:27:10 -0700

    feat: add styles to minerals list

--- app/(tabs)/minerals/index.tsx
+++ app/(tabs)/minerals/index.tsx
@@ -1,7 +1,8 @@
 import ParallaxScrollView from '@/components/ParallaxScrollView';
+import { ThemedText } from '@/components/ThemedText';
 import { Link } from 'expo-router';
 import { useEffect, useState } from 'react';
-import { ActivityIndicator, FlatList, StyleSheet, Text, View } from 'react-native';
+import { ActivityIndicator, FlatList, Image, StyleSheet, View } from 'react-native';
 
 export default function HomeScreen() {
     const [minerals, setMinerals] = useState<any[]>([]);
@@ -36,13 +37,21 @@ export default function HomeScreen() {
             headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
         >
             <View style={styles.container}>
-                <Text>Minerals</Text>
+                <ThemedText>Minerals</ThemedText>
                 <FlatList
                     data={minerals}
                     keyExtractor={(item) => item.id?.toString() ?? item.name}
+                    style={{ alignSelf: 'stretch' }}
+                    contentContainerStyle={{ paddingBottom: 16 }}
                     renderItem={({ item }) => (
-                        <Link href={`/minerals/${item.id}`}>
-                            <Text>{item.name}</Text>
+                        <Link href={`/minerals/${item.id}`} asChild>
+                            <View style={styles.itemRow}>
+                                <Image
+                                    source={{ uri: item.photos[0].photo.image || 'https://via.placeholder.com/60' }}
+                                    style={styles.itemImage}
+                                />
+                                <ThemedText style={styles.itemName}>{item.name}</ThemedText>
+                            </View>
                         </Link>
                     )}
                 />
@@ -55,6 +64,27 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         justifyContent: 'center',
+    },
+    itemRow: {
+        flexDirection: 'row',
         alignItems: 'center',
+        paddingVertical: 10,
+        borderRadius: 8,
+        marginVertical: 4,
+        shadowColor: '#000',
+        shadowOpacity: 0.05,
+        shadowRadius: 2,
+        elevation: 1,
+        alignSelf: 'stretch',
+    },
+    itemImage: {
+        width: 100,
+        height: 66.67,
+        borderRadius: 8,
+        marginRight: 16,
+    },
+    itemName: {
+        fontSize: 18,
+        flexShrink: 1,
     },
 });
