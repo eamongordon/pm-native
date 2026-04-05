e3fe2355f9dbb53fd0b00bf70f0729f85194341d
Eamon Gordon <ekeokigordon@icloud.com>
2026-04-05 16:29:16 -0400

    fix: modify plugin and add 16kb libraries

--- withAndroid16KBSupport.js
+++ withAndroid16KBSupport.js
@@ -2,18 +2,18 @@ const { withAppBuildGradle, withAndroidManifest } = require('@expo/config-plugin
 
 /**
  * 16 KB Memory Page Size Support Plugin for EAS Managed Builds
- * - Forces Fresco 3.2.0 (16 KB aligned)
- * - Adds doNotStrip for core Expo and Fresco modules
- * - Sets extractNativeLibs="true" in AndroidManifest
  */
 const withAndroid16KBSupport = (config) => {
-  // 1. Update build.gradle (Fresco version and doNotStrip)
-  config = withAppBuildGradle(config, (config) => {
-    const content = config.modResults.contents;
+  // 1. Force 16 KB compliance for all native modules
+  config = withAppBuildGradle(config, (configWithProps) => {
+    const contents = configWithProps.modResults.contents;
+    if (!contents) {
+      return configWithProps;
+    }
 
-    if (!content.includes('fresco:3.2.0')) {
-      config.modResults.contents += `
-// [Added by withAndroid16KBSupport] Force Fresco 3.2.0 for 16 KB alignment
+    if (!contents.includes('16KB_ALIGNMENT_FIX_SIGNATURE')) {
+      configWithProps.modResults.contents += `
+// [16KB_ALIGNMENT_FIX_SIGNATURE] Ensure 16 KB compliance for core and third-party natives
 android {
     configurations.all {
         resolutionStrategy {
@@ -22,24 +22,33 @@ android {
             force 'com.facebook.fresco:webpsupport:3.2.0'
             force 'com.facebook.fresco:animated-webp:3.2.0'
             force 'com.facebook.fresco:imagepipeline-okhttp3:3.2.0'
+            force 'com.facebook.fresco:fbcore:3.2.0'
+            force 'com.facebook.fresco:imagepipeline:3.2.0'
+            force 'com.facebook.fresco:imagepipeline-base:3.2.0'
+            force 'com.facebook.fresco:drawee:3.2.0'
+            force 'com.facebook.fresco:imagepipeline-native:3.2.0'
+            force 'com.facebook.soloader:soloader:0.11.0'
         }
     }
-    packagingOptions {
-        doNotStrip "**/libexpo-modules-core.so"
-        doNotStrip "**/libanimation-decoder-gif.so"
-        doNotStrip "**/libavif_android.so"
+    packaging {
+        jniLibs {
+            useLegacyPackaging = false
+            doNotStrip "**/*.so"
+        }
     }
 }
-      `;
+`;
     }
-    return config;
+    return configWithProps;
   });
 
   // 2. Update AndroidManifest (extractNativeLibs)
-  config = withAndroidManifest(config, (config) => {
-    const mainApplication = config.modResults.manifest.application[0];
-    mainApplication.$['android:extractNativeLibs'] = 'true';
-    return config;
+  config = withAndroidManifest(config, (configWithProps) => {
+    const mainApplication = configWithProps.modResults.manifest.application[0];
+    if (mainApplication && mainApplication.$) {
+      mainApplication.$['android:extractNativeLibs'] = 'true';
+    }
+    return configWithProps;
   });
 
   return config;
